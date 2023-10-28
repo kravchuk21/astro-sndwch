9f858e001757ec879bcb781b92e40563a35251d8
kravchuk21 <krauchukvlad@gmail.com>
2023-10-28 19:03:19 +0300

    fix:  config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-frontend-masters.netlify-kravchuk.app',
+	site: 'https://astro-frontend-masters-kravchuk.netlify.app',
 	output: 'hybrid',
 	experimental: {
 		hybridOutput: true,
